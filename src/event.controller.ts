6661c592494c9d900c08d2a5f6b6da58d1dab658
unknown <39314465+BigBlueDraco@users.noreply.github.com>
2024-02-27 15:37:08 +0200

    add: CreateEventDto

--- src/event.controller.ts
+++ src/event.controller.ts
@@ -1,18 +1,37 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateEventDto } from './create-event.dto';
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {}
+  findAll() {
+    return {
+      id: 1,
+      name: 'Danylo',
+    };
+  }
   @Get('/:id')
   findOne(@Param('id') id) {
-    return id;
+    return {
+      id,
+      name: 'Danylo',
+    };
   }
   @Post()
-  create() {}
+  create(@Body() body: CreateEventDto) {
+    return body;
+  }
   @Patch('/:id')
-  update(@Param('id') id) {
-    return id;
+  update(@Param('id') id, @Body() body) {
+    return { id, ...body };
   }
   @Delete('/:id')
   remove(@Param('id') id) {
