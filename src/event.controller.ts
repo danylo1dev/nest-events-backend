5583ef40d541d2fd5bf4db0124f19422a1c7167a
unknown <39314465+BigBlueDraco@users.noreply.github.com>
2024-02-27 17:23:28 +0200

    add: docker

--- src/event.controller.ts
+++ src/event.controller.ts
@@ -3,39 +3,52 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { CreateEventDto } from './create-event.dto';
 import { UpdateEventDto } from './update-event.dto';
+import { Event } from './event.entity';
 
 @Controller('/events')
 export class EventsController {
+  private events: Event[] = [];
   @Get()
   findAll() {
-    return {
-      id: 1,
-      name: 'Danylo',
-    };
+    return this.events;
   }
   @Get('/:id')
   findOne(@Param('id') id) {
-    return {
-      id,
-      name: 'Danylo',
-    };
+    return this.events.find((event) => event.id === +id);
   }
   @Post()
   create(@Body() body: CreateEventDto) {
-    return body;
+    const event = {
+      ...body,
+      when: new Date(body.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
+    return event;
   }
   @Patch('/:id')
   update(@Param('id') id, @Body() body: UpdateEventDto) {
-    return { id, ...body };
+    const index = this.events.findIndex((event) => event.id === +id);
+    this.events[index] = {
+      ...this.events[index],
+      ...body,
+      when: body.when ? new Date(body.when) : this.events[index].when,
+    };
+    return this.events[index];
   }
   @Delete('/:id')
+  @HttpCode(204)
   remove(@Param('id') id) {
+    this.events = this.events.filter((event) => {
+      return event.id !== +id;
+    });
     return id;
   }
 }
