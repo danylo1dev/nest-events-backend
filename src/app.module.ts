59fbf368a2fceacdc5a3115014175d14812f101c
unknown <39314465+BigBlueDraco@users.noreply.github.com>
2024-02-28 12:01:05 +0200

    add: relationals to events

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Event } from './events/event.entity';
-import { EventsModule } from './events/events.module';
 import ormConfig from './config/orm.config';
-import ormConfigProd from './config/orm.config.prod';
+import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
@@ -16,8 +14,7 @@ import ormConfigProd from './config/orm.config.prod';
       expandVariables: true,
     }),
     TypeOrmModule.forRootAsync({
-      useFactory:
-        process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
+      useFactory: ormConfig,
     }),
     EventsModule,
   ],
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsController } from './event.controller';
+import { Attendee } from './attendee';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event])],
+  imports: [TypeOrmModule.forFeature([Event, Attendee])],
   controllers: [EventsController],
 })
 export class EventsModule {}
