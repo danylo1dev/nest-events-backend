e4ab21d5003ccf0016c5b311b00f77f40ed47235
Danylo Hlushko <danylo.hlushko.dev@gmail.com>
2024-03-08 11:55:51 +0200

    ref auth

--- src/config/auth-jwt.config.ts
+++ src/config/auth-jwt.config.ts
@@ -6,7 +6,7 @@ export default registerAs(
   (): JwtModuleOptions => ({
     secret: process.env.JWT_SECRET,
     signOptions: {
-      expiresIn: process.env.JWT_SECRET,
+      expiresIn: process.env.JWT_EXPIRES_IN,
     },
   }),
 );
